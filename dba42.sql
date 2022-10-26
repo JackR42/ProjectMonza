c263de3cee9d0ab1d785ccfa2a786f583923aa50
JackR42 <j.roumimper@gmail.com>
2022-10-26 14:16:40 +0200

    Update dba42.sql

--- dba42.sql
+++ dba42.sql
@@ -1,1 +1,12 @@
 select ServerName=@@ServerName, DatabaseName=DB_NAME(), UserName=suser_sname()
+go
+create table t42(
+   c1 int primary key,
+   c2 char(32)
+);
+go
+insert into t42 values(1, 'Monza')
+insert into t42 values(2, 'Fortytwo')
+go
+select * from dbo.t42
+go
